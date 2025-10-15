b0f4138006cf11e1ffb34cdd16877d9d1f9ce027
Neto <wantedshark34@gmail.com>
2025-10-14 22:05:32 -0300

    hoome page created at nossos pilares and roboto font added

--- src/pages/inicio/Inicio.tsx
+++ src/pages/inicio/Inicio.tsx
@@ -1,8 +1,116 @@
-import "./inicio.css"
+import "./inicio.css";
+import quad from "../../assets/quadriculado.png";
+import phone from "../../assets/celular.png";
+import eficiencia from "../../assets/eficiencia.png";
+import conectividade from "../../assets/conectividade.png";
+import transformacao from "../../assets/transformacao.png";
+import aula from "../../assets/aula.png";
+import acompanhamento from "../../assets/acompanhamento.png";
+import conectividade2 from "../../assets/conectividade2.png";
 
 export default function Inicio() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <div className="inicio">
+      <div className="inicio-banner">
+        <div
+          className="banner-img"
+          style={{ backgroundImage: `url(${quad})` }}
+        ></div>
+        <div className="banner-texts">
+          <h3 className="banner-texts-title">
+            Transformando a <br /> Educação com Inovação e Conexão
+          </h3>
+          <p className="banner-texts-desc">
+            O Geduc conecta mais de 350 mil alunos, 30 mil professores e 2 mil
+            instituições, oferecendo uma gestão eficiente e uma educação mais
+            acessível e transformadora.
+          </p>
+          <div className="banner-buttons">
+            <button className="button">Conheça a Plataforma</button>
+            <button className="button button--inverted">Seja Parceiro</button>
+          </div>
+        </div>
+        <div className="banners-phone">
+          <div className="phone-circle1"></div>
+          <img src={phone} alt="celular" className="phone-img" />
+          <div className="phone-circle2"></div>
+        </div>
+      </div>
+      <div className="inicio-descriptions">
+        <div className="inicio-description">
+          <div className="inicio-description-img">
+            <img src={conectividade} alt="" />
+          </div>
+          <h4 className="inicio-description-title">Conectividade</h4>
+          <p className="inicio-description-text">
+            Integra alunos, professores e instituições
+          </p>
+        </div>
+        <div className="inicio-description">
+          <div className="inicio-description-img">
+            <img src={eficiencia} alt="" />
+          </div>
+          <h4 className="inicio-description-title">Eficiência</h4>
+          <p className="inicio-description-text">Automação e gestão completa</p>
+        </div>
+        <div className="inicio-description">
+          <div className="inicio-description-img">
+            <img src={transformacao} alt="" />
+          </div>
+          <h4 className="inicio-description-title">Transformação</h4>
+          <p className="inicio-description-text">
+            Educação moderna e acessivel
+          </p>
+        </div>
+      </div>
+      <div className="inicio-pillars">
+        <div className="inicio-pillars-header">
+          <div className="pillars-header-texts">
+            <p className="pillars-header-text">
+              <span className="pillars-header-line"></span> Nossos Pilares
+            </p>
+            <h2 className="pillars-header-title">Conheça os nossos Pilares</h2>
+            <p className="pillars-header-desc">O Que Nos Torna Únicos</p>
+          </div>
+          <div className="pillars-header-button">
+            <button className="button button--inverted button--small">
+              Saiba Mais
+            </button>
+          </div>
+        </div>
+        <div className="inicio-pillars-cards">
+          <div className="pillars-card">
+            <div className="pillars-card-texts">
+              <h4 className="pillars-card-title"></h4>
+              <p className="pillars-card-desc"></p>
+            </div>
+            <img className="pillars-card-img" src={aula} alt="" />
+          </div>
+          <div className="pillars-card">
+            <div className="pillars-card-texts">
+              <h4 className="pillars-card-title">
+                Acompanhamento e Diagnósticos
+              </h4>
+              <p className="pillars-card-desc">
+                Mapear pontos fortes e dificuldades com base nas habilidades dos
+                alunos permitindo que os professores avaliem o desempenho dos
+                alunos.
+              </p>
+            </div>
+            <img className="pillars-card-img" src={acompanhamento} alt="" />
+          </div>
+          <div className="pillars-card">
+            <div className="pillars-card-texts">
+            <h4 className="pillars-card-title">Conectividade</h4>
+              <p className="pillars-card-desc">
+                Manter os alunos engajados mesmo fora da escola para desenvolver
+                aprendizagem colaborativa.
+              </p>
+            </div>
+            <img className="pillars-card-img" src={conectividade2} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
